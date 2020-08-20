876a30ca67e77aee06579361afd98f88b7abdec3
zhangjiahao <1643187898@qq.com>
2020-08-20 21:41:45 +0800

    作业

--- src/main.js
+++ src/main.js
@@ -2,10 +2,14 @@ import Vue from 'vue'
 import ElementUI from 'element-ui'
 import App from './App.vue'
 import router from './router'
+import request from './utils/request';
 import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.use(ElementUI);
-Vue.config.productionTip = false
+Vue.prototype.$request = request
+
+
+
 
 new Vue({
   router,
--- src/views/User.vue
+++ src/views/User.vue
@@ -1,11 +1,72 @@
 <template>
-    <div>
-        用户管理
-        <router-view/>
-    </div>
+    <el-table
+    :data="tableData.filter(data => !search || data.name.toLowerCase().includes(search.toLowerCase()))"
+    style="width: 100%">
+      <el-table-column type="selection" width="55"></el-table-column>
+      <el-table-column type="index" label="#" width="55"></el-table-column>
+      <el-table-column label="用户名" prop="username"></el-table-column>
+      <el-table-column prop="age" label="年龄" width="120"></el-table-column>
+      <el-table-column prop="gender" label="性别" width="120" show-overflow-tooltip></el-table-column>
+
+    <el-table-column
+      align="right">
+      <template slot="header">
+        <el-input
+          v-model="search"
+          size="mini"
+          placeholder="输入关键字搜索"/>
+      </template>
+      <template slot-scope="scope">
+        <el-button
+          size="mini"
+          @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+        <el-button
+          size="mini"
+          type="danger"
+          @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+      </template>
+    </el-table-column>
+  </el-table>
 </template>
 <script>
-export default {
+  export default {
+    data() {
+      return {
+        userlist: [],
+        currentId: "",
+      }
+    },
+    methods: {
+    async handleDelete(id) {
+      this.$confirm("你确认要删除这条数据吗", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning",
+      }).then(async () => {
+        const { data } = await this.$request.delete("/user/" + id);
+
+        if (data.code === 1) {
+          this.userlist = this.userlist.filter((item) => item._id !== id);
+          this.$message({
+            type: "success",
+            message: "删除成功!",
+          });
+        }
+      });
+    },
+    },
+      async created() {
+    // console.log("List=", this);
+    // axios({
+    //     url:'http://47.112.180.216:2003/api/user',
+    //     method:'get',
     
-}
+    // })
+    
+    const { data } = await this.$request.get("/user");
+    
+
+    this.userlist = data.data;
+  },
+  }
 </script>
