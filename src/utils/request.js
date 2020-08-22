f6b2bee7746df6a18d36ff4db59ca82928c935c0
tikUI <1518491547@qq.com>
2020-08-22 08:59:44 +0800

    项目更新

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 
 const request = axios.create({
-	baseURL: 'http://47.112.180.216:2003/api'
+	baseURL: 'http://47.112.180.216:2003/api',
+	headers:{"Content-Type":"application/json"}
 });
 
 export default request;
